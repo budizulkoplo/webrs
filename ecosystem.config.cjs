4d090b18158d04a92098384bdf2f11f055bac3c1
Dzulfikar Budi Andreanto <buddyandreanto@gmail.com>
2026-07-13 11:07:16 +0700

    Run PM2 with standalone server

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'webrs',
-      script: 'npm',
-      args: 'run start',
+      script: '.next/standalone/server.js',
       env: {
         NODE_ENV: 'production',
+        PORT: '8080',
         TZ: 'Asia/Jakarta',
       },
     },
